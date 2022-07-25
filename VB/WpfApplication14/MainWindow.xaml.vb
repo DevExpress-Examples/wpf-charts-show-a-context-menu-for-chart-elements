a5b615d77543e0dd3ec2b6041a354d8bc1f4bf68
DevExpressExampleBot <fakeEmail>
2022-07-25 16:52:16 +0400

    Source auto update [skip ci]

--- VB/WpfApplication14/MainWindow.xaml.vb
+++ VB/WpfApplication14/MainWindow.xaml.vb
@@ -1,53 +1,42 @@
-﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Charts
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
 Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication14
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-
+            Me.InitializeComponent()
         End Sub
 
-        Private Sub ccmHide_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            Dim menu As PopupMenu = TryCast(DirectCast(sender, BarItem).Parent, PopupMenu)
-            Dim targetObjects() As Object = TryCast(menu.Tag, Object())
+        Private Sub ccmHide_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            Dim menu As PopupMenu = TryCast(CType(sender, BarItem).Parent, PopupMenu)
+            Dim targetObjects As Object() = TryCast(menu.Tag, Object())
             Dim series As Series = TryCast(targetObjects(0), Series)
             series.Visible = False
         End Sub
 
-        Private Sub ccmInfo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            Dim menu As PopupMenu = TryCast(DirectCast(sender, BarItem).Parent, PopupMenu)
-            Dim targetObjects() As Object = TryCast(menu.Tag, Object())
+        Private Sub ccmInfo_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            Dim menu As PopupMenu = TryCast(CType(sender, BarItem).Parent, PopupMenu)
+            Dim targetObjects As Object() = TryCast(menu.Tag, Object())
             Dim series As Series = TryCast(targetObjects(0), Series)
-            Me.Title = series.DisplayName
+            Title = series.DisplayName
         End Sub
 
-
         Private Sub PopupMenu_Opening(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs)
-            Dim hitInfo = chartControl1.CalcHitInfo(Mouse.GetPosition(Application.Current.MainWindow))
+            Dim hitInfo = Me.chartControl1.CalcHitInfo(Mouse.GetPosition(Application.Current.MainWindow))
             If Not hitInfo.InSeries Then
                 e.Cancel = True
             Else
-                Dim menu As PopupMenu = DirectCast(sender, PopupMenu)
-                menu.Tag = New Object() { hitInfo.Series }
+                Dim menu As PopupMenu = CType(sender, PopupMenu)
+                menu.Tag = New Object() {hitInfo.Series}
             End If
         End Sub
     End Class
